24d9aabd64b796584f10b8916a6f6d2ceaa749af
Mathieu Acthernoene <zoontek@gmail.com>
2026-08-05 17:33:26 +0200

    Remove feature-detection branches

--- packages/react-native-web/src/modules/useElementLayout/index.js
+++ packages/react-native-web/src/modules/useElementLayout/index.js
@@ -16,48 +16,34 @@ import canUseDOM from '../canUseDom';
 
 const DOM_LAYOUT_HANDLER_NAME = '__reactLayoutHandler';
 
-let didWarn = !canUseDOM;
 let resizeObserver = null;
 
 function getResizeObserver() /*: ?ResizeObserver */ {
-  if (canUseDOM && typeof window.ResizeObserver !== 'undefined') {
-    if (resizeObserver == null) {
-      resizeObserver = new window.ResizeObserver(function (entries) {
-        entries.forEach((entry) => {
-          const node = entry.target;
-          const onLayout = node[DOM_LAYOUT_HANDLER_NAME];
-          if (typeof onLayout === 'function') {
-            // We still need to measure the view because browsers don't yet provide
-            // border-box dimensions in the entry
-            UIManager.measure(node, (x, y, width, height, left, top) => {
-              const event /*: LayoutEvent */ = {
-                // $FlowFixMe
-                nativeEvent: {
-                  layout: { x, y, width, height, left, top }
-                },
-                timeStamp: Date.now()
-              };
-              Object.defineProperty(event.nativeEvent, 'target', {
-                enumerable: true,
-                get: () => entry.target
-              });
-              onLayout(event);
+  if (canUseDOM && resizeObserver == null) {
+    resizeObserver = new window.ResizeObserver(function (entries) {
+      entries.forEach((entry) => {
+        const node = entry.target;
+        const onLayout = node[DOM_LAYOUT_HANDLER_NAME];
+        if (typeof onLayout === 'function') {
+          // We still need to measure the view because browsers don't yet provide
+          // border-box dimensions in the entry
+          UIManager.measure(node, (x, y, width, height, left, top) => {
+            const event /*: LayoutEvent */ = {
+              // $FlowFixMe
+              nativeEvent: {
+                layout: { x, y, width, height, left, top }
+              },
+              timeStamp: Date.now()
+            };
+            Object.defineProperty(event.nativeEvent, 'target', {
+              enumerable: true,
+              get: () => entry.target
             });
-          }
-        });
+            onLayout(event);
+          });
+        }
       });
-    }
-  } else if (!didWarn) {
-    if (
-      process.env.NODE_ENV !== 'production' &&
-      process.env.NODE_ENV !== 'test'
-    ) {
-      console.warn(
-        'onLayout relies on ResizeObserver which is not supported by your browser. ' +
-          'Please include a polyfill, e.g., https://github.com/que-etc/resize-observer-polyfill.'
-      );
-      didWarn = true;
-    }
+    });
   }
   return resizeObserver;
 }
