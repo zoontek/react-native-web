2151f11dc2692c026937ecbd1ccd5a9a4ff9a88a
Mathieu Acthernoene <zoontek@gmail.com>
2026-08-10 16:30:11 +0200

    Delete ProgressBar

--- packages/react-native-web/src/index.js
+++ packages/react-native-web/src/index.js
@@ -41,7 +41,6 @@ export { default as KeyboardAvoidingView } from './exports/KeyboardAvoidingView'
 export { default as Modal } from './exports/Modal';
 export { default as Picker } from './exports/Picker';
 export { default as Pressable } from './exports/Pressable';
-export { default as ProgressBar } from './exports/ProgressBar';
 export { default as RefreshControl } from './exports/RefreshControl';
 export { default as SafeAreaView } from './exports/SafeAreaView';
 export { default as ScrollView } from './exports/ScrollView';
