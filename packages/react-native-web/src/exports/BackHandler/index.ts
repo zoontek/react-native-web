1a15673f4c78b54e7bc6e912dd76a3dca3f47962
Mathieu Acthernoene <zoontek@gmail.com>
2026-08-24 15:59:06 +0200

    Clipboard

--- packages/react-native-web/src/exports/BackHandler/index.ts
+++ packages/react-native-web/src/exports/BackHandler/index.ts
@@ -10,6 +10,7 @@ import type * as RN from 'react-native';
 
 const BackHandler: typeof RN.BackHandler = {
   exitApp() {},
+
   addEventListener() {
     console.error(
       'BackHandler is not supported on web and should not be used.'
--- packages/react-native-web/src/exports/Clipboard/index.ts
+++ packages/react-native-web/src/exports/Clipboard/index.ts
@@ -8,27 +8,29 @@
 
 'use client';
 
+import type * as RN from 'react-native';
+
 let clipboardAvailable: boolean | undefined;
 
-export default class Clipboard {
-  static isAvailable(): boolean {
-    if (clipboardAvailable === undefined) {
+const Clipboard: typeof RN.Clipboard = class {
+  static isAvailable() {
+    if (clipboardAvailable == null) {
       clipboardAvailable =
         typeof document.queryCommandSupported === 'function' &&
         document.queryCommandSupported('copy');
     }
+
     return clipboardAvailable;
   }
 
-  static getString(): Promise<string> {
+  static getString() {
     return Promise.resolve('');
   }
 
-  static setString(text: string): boolean {
-    let success = false;
+  static setString = (text): void => {
     const body = document.body;
 
-    if (body) {
+    if (body != null) {
       // add the text to a hidden node
       const node = document.createElement('span');
       node.textContent = text;
@@ -50,7 +52,6 @@ export default class Clipboard {
         // attempt to copy
         try {
           document.execCommand('copy');
-          success = true;
         } catch {}
 
         // remove selection
@@ -60,7 +61,7 @@ export default class Clipboard {
       // remove node
       body.removeChild(node);
     }
+  };
+};
 
-    return success;
-  }
-}
+export default Clipboard;
