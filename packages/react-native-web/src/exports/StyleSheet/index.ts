685ecb4a6dbeb5ee5371d20346154d19057e914a
Mathieu Acthernoene <zoontek@gmail.com>
2026-08-26 17:30:04 +0200

    StyleSheet

--- packages/react-native-web/src/exports/StyleSheet/index.ts
+++ packages/react-native-web/src/exports/StyleSheet/index.ts
@@ -5,6 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+import type * as RN from 'react-native';
 import { styleq, type StyleqResult, type Styles } from 'styleq';
 import {
   localizeStyle,
@@ -12,7 +13,7 @@ import {
 } from 'styleq/transform-localize-style';
 
 import canUseDOM from '../../modules/canUseDom';
-import type { GenericStyleProp } from '../../types';
+import type { Except, GenericStyleProp } from '../../types';
 import { atomic, classic, inline } from './compiler';
 import type { Style } from './compiler/createReactDOMStyle';
 import type { StyleValue } from './compiler/normalizeValueWithProperty';
@@ -35,6 +36,7 @@ function customStyleq(
 ): StyleqResult {
   const { writingDirection, ...preprocessOptions } = options;
   const isRTL = writingDirection === 'rtl';
+
   return styleq.factory({
     transform(style) {
       const compiledStyle = staticStyleMap.get(style);
@@ -75,15 +77,9 @@ function compileAndInsertReset(style: Style, key: string) {
 
 /* ----- API ----- */
 
-const absoluteFillObject = {
-  position: 'absolute',
-  left: 0,
-  right: 0,
-  top: 0,
-  bottom: 0
-};
-
-const absoluteFill = create({ x: { ...absoluteFillObject } }).x;
+const absoluteFill = create({
+  x: { position: 'absolute', left: 0, right: 0, top: 0, bottom: 0 }
+}).x;
 
 /**
  * create
@@ -93,9 +89,11 @@ function create<const T extends Record<string, StyleObject>>(
 ): Readonly<T> {
   Object.keys(styles).forEach((key) => {
     const styleObj = styles[key];
+
     // Only compile at runtime if the style is not already compiled
     if (styleObj != null && styleObj.$$css !== true) {
       let compiledStyles;
+
       if (key.indexOf('$raw') > -1) {
         compiledStyles = compileAndInsertReset(
           styleObj as Style,
@@ -107,11 +105,14 @@ function create<const T extends Record<string, StyleObject>>(
           (styles as Record<string, StyleObject>)[key] =
             Object.freeze(styleObj);
         }
+
         compiledStyles = compileAndInsertAtomic(styleObj as Style);
       }
+
       staticStyleMap.set(styleObj, compiledStyles as LocalizedStyle);
     }
   });
+
   return styles;
 }
 
@@ -121,9 +122,11 @@ function create<const T extends Record<string, StyleObject>>(
 function compose<T, U>(style1: T, style2: U): [T, U] {
   if (process.env.NODE_ENV !== 'production') {
     const len = arguments.length;
+
     if (len > 2) {
       // oxlint-disable-next-line prefer-rest-params
       const readableStyles = [...arguments].map((a) => flatten(a));
+
       throw new Error(
         `StyleSheet.compose() only accepts 2 arguments, received ${len}: ${JSON.stringify(
           readableStyles
@@ -131,21 +134,23 @@ function compose<T, U>(style1: T, style2: U): [T, U] {
       );
     }
   }
+
   return [style1, style2];
 }
 
 /**
  * flatten
  */
-function flatten(...styles: Array<unknown>): Style {
-  const flatArray = styles.flat(Infinity);
+function flatten(style: unknown): Style {
+  const flatArray = [style].flat(Infinity);
   const result: Style = {};
-  for (let i = 0; i < flatArray.length; i++) {
-    const style = flatArray[i];
+
+  for (const style of flatArray) {
     if (style != null && typeof style === 'object') {
       Object.assign(result, style);
     }
   }
+
   return result;
 }
 
@@ -153,10 +158,7 @@ function flatten(...styles: Array<unknown>): Style {
  * getSheet
  */
 function getSheet(): { id: string; textContent: string } {
-  return {
-    id: sheet.id,
-    textContent: sheet.getTextContent()
-  };
+  return { id: sheet.id, textContent: sheet.getTextContent() };
 }
 
 /**
@@ -170,27 +172,19 @@ export type Options = {
   writingDirection?: 'auto' | 'ltr' | 'rtl';
 };
 
-function StyleSheet(
+function resolve(
   styles: GenericStyleProp<StyleObject>,
   options: Options = {}
 ): StyleProps {
   const isRTL = options.writingDirection === 'rtl';
   const styleProps: StyleProps = customStyleq(styles, options);
+
   if (Array.isArray(styleProps) && styleProps[1] != null) {
     styleProps[1] = inline(styleProps[1], isRTL);
   }
+
   return styleProps;
 }
-
-StyleSheet.absoluteFill = absoluteFill;
-StyleSheet.absoluteFillObject = absoluteFillObject;
-StyleSheet.create = create;
-StyleSheet.compose = compose;
-StyleSheet.flatten = flatten;
-StyleSheet.getSheet = getSheet;
-// `hairlineWidth` is not implemented using screen density as browsers may
-// round sub-pixel values down to `0`, causing the line not to be rendered.
-StyleSheet.hairlineWidth = 1;
 
 declare global {
   interface Window {
@@ -199,20 +193,80 @@ declare global {
 }
 
 if (canUseDOM && window.__REACT_DEVTOOLS_GLOBAL_HOOK__) {
-  window.__REACT_DEVTOOLS_GLOBAL_HOOK__.resolveRNStyle = StyleSheet.flatten;
+  window.__REACT_DEVTOOLS_GLOBAL_HOOK__.resolveRNStyle = flatten;
 }
 
-export type IStyleSheet = {
-  (styles?: GenericStyleProp<StyleObject>, options?: Options): StyleProps;
+const Stylesheet: Except<
+  typeof RN.StyleSheet,
+  'absoluteFill' | 'create' | 'compose' | 'flatten'
+> & {
+  /**
+   * A very common pattern is to create overlays with position absolute and zero positioning,
+   * so `absoluteFill` can be used for convenience and to reduce duplication of these repeated
+   * styles.
+   */
   absoluteFill: StyleObject;
-  absoluteFillObject: StyleObject;
+
+  /**
+   * Compiles the styles into CSS rules, inserts them into the style sheet,
+   * and returns the given object.
+   */
   create: typeof create;
+
+  /**
+   * Combines two styles such that style2 will override any styles in style1.
+   * If either style is falsy, the other one is returned without allocating
+   * an array, saving allocations and maintaining reference equality for
+   * PureComponent checks.
+   */
   compose: typeof compose;
+
+  /**
+   * Flattens an array of style objects, into one aggregated style object.
+   *
+   * Example:
+   * ```
+   * const styles = StyleSheet.create({
+   *   listItem: {
+   *     flex: 1,
+   *     fontSize: 16,
+   *     color: 'white'
+   *   },
+   *   selectedListItem: {
+   *     color: 'green'
+   *   }
+   * });
+   *
+   * StyleSheet.flatten([styles.listItem, styles.selectedListItem])
+   * // returns { flex: 1, fontSize: 16, color: 'green' }
+   * ```
+   */
   flatten: typeof flatten;
+
+  /**
+   * Resolves style objects to the `className` and `style` props to apply
+   * to a DOM element.
+   */
+  resolve: typeof resolve;
+
+  /**
+   * Returns the id and the text content of the style sheet, so it can be
+   * inserted into the HTML during server-side rendering.
+   */
   getSheet: typeof getSheet;
-  hairlineWidth: number;
+} = {
+  // `hairlineWidth` is not implemented using screen density as browsers may
+  // round sub-pixel values down to `0`, causing the line not to be rendered.
+  hairlineWidth: 1,
+  setStyleAttributePreprocessor() {},
+
+  absoluteFill,
+  create,
+  compose,
+  flatten,
+
+  resolve,
+  getSheet
 };
 
-const stylesheet: IStyleSheet = StyleSheet;
-
-export default stylesheet;
+export default Stylesheet;
