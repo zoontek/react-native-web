facb009efbe51d6cbe8310d6aa6709aec345f446
Mathieu Acthernoene <zoontek@gmail.com>
2026-08-28 12:28:44 +0200

    Update unitless numbers list

--- packages/react-native-web/src/exports/StyleSheet/compiler/normalizeValueWithProperty.ts
+++ packages/react-native-web/src/exports/StyleSheet/compiler/normalizeValueWithProperty.ts
@@ -5,7 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import unitlessNumbers from '../../../modules/unitlessNumbers';
+import isUnitlessNumber from '../../../modules/isUnitlessNumber';
 import type { Nullable } from '../../../types';
 import normalizeColor from './normalizeColor';
 
@@ -32,7 +32,7 @@ export default function normalizeValueWithProperty<
   T extends StyleValue | null | undefined
 >(value: T, property?: Nullable<string>): T | string | undefined {
   if (
-    (property == null || !unitlessNumbers[property]) &&
+    (property == null || !isUnitlessNumber(property)) &&
     typeof value === 'number'
   ) {
     return `${value}px`;
